5774a518cf17c11394c3cc35e50eba3eb4608494
David Lee <david@lee269.co.uk>
2017-12-23 22:10:49 +0000

    Trying to implement purrr

--- R/scratchpad.R
+++ R/scratchpad.R
@@ -8,8 +8,8 @@ library(dplyr)
 baseurl <- "https://lapada.org/art-and-antiques/set-of-french-side-tables-in-lacquered-and-painted-chinoiserie-wood-20th-century/"
 html <- read_html(baseurl)
 
-objectdescription <- ":nth-child(3) .page-content p"
-objectcondition <- ":nth-child(4) p"
+objectdescription <- ":nth-child(3) .page-content"
+objectcondition <- ":nth-child(4) .page-content"
 material <- "li:nth-child(1) .px"
 dimensions <- "li:nth-child(1) .px"
 country <- "li:nth-child(4) .px a"
@@ -24,9 +24,21 @@ urls <- as.list(ld$url[1:10])
 
 html <- urls %>% map(read_html) 
 
-df <- html %>% 
-  map(html_nodes, ":nth-child(3) .page-content p") %>% 
-  map_df(map_df, ~list(var = .x %>% html_node(":nth-child(3) .page-content p") %>% html_text()))
+
+objdesc <- html %>% 
+  map(html_nodes, ":nth-child(3) .page-content") %>% 
+  map_df(map_df, ~list(var = .x %>%  html_text()))
+
+objcond <- html %>% 
+  map(html_nodes, ":nth-child(4) .page-content") %>% 
+  map_df(map_df, ~list(var = .x %>%  html_text()))
+
+mat <- html %>% 
+  map(html_nodes, "li:nth-child(1) .px") %>% 
+  map_df(map_df, ~list(var = .x %>%  html_text()))
+
+
+df <- data.frame(ld$url[1:10], objdesc, objcond, mat)
 
 # Interesting: https://stackoverflow.com/questions/43035598/replacing-missing-value-when-web-scraping-rvest
 # git remote add origin https://github.com/lee269/Ivory.git
